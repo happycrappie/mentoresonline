a05a8f72384e3736cb52551a8a5b42dcc0cd9401
Eryc Silva <imerycdasilva@gmail.com>
2019-11-05 11:24:22 -0300

    :wrench: Update NPM, Gulpfile and fix vulnerabilities.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 /*****************
  * Gulp Requires
  *****************/
-  const { src, dest, series, parallel } = require('gulp'),
+  const { src, dest, watch, series, parallel } = require('gulp'),
         pug = require('gulp-pug'),
         sass = require('gulp-sass'),
+        gulpif = require('gulp-if'),
         bs = require('browser-sync'),
         babel = require('gulp-babel'),
         clean = require('gulp-clean'),
-        concat = require('gulp-concat'),
+        // concat = require('gulp-concat'),
         plumber = require('gulp-plumber'),
-        cleanCSS = require('gulp-clean-css'),
-        prefix = require('gulp-autoprefixer'),
-        concatCSS = require('gulp-concat-css');
+        imagemin = require('gulp-imagemin'),
+        // cleanCSS = require('gulp-clean-css'),
+        prefix = require('gulp-autoprefixer');
+        // concatCSS = require('gulp-concat-css');
 
+  const critical = require('critical').stream;
   const reload = bs.reload;
 
 /*****************
@@ -29,6 +32,7 @@
     css: srcRoot + 'sass/',
     mail: srcRoot + 'mail/',
     views: srcRoot + 'views/',
+    img: srcRoot + 'assets/img/',
     pages: srcRoot + 'views/pages/',
     videos: srcRoot + 'views/videos/',
     assets: srcRoot + 'assets/',
@@ -49,19 +53,23 @@
     assets: buildRoot + '_assets/',
   }
 
+var env = process.env.NODE_ENV;
+
 /*****************
  * Views
  *****************/
   function views() {
 
     return src([
-        views + '*.pug',
-        pages + '**/*.pug',
-        videos + '**/*.pug'
+        srcPaths.views + '*.pug',
+        srcPaths.pages + '**/*.pug',
+        srcPaths.videos + '**/*.pug'
       ])
-      .pipe(pug({
+      .pipe(gulpif(env === 'production', pug({
+        pretty: false
+      }), pug({
         pretty: true
-      }))
+      })))
       .pipe(plumber())
       .pipe(dest(buildRoot));
   }
@@ -69,33 +77,34 @@
 /*****************
  * Styles
  *****************/
-  function styles(cb) {
+  function styles() {
 
     // Main Styles
-    src(srcPaths.css + 'main.scss')
+    return src(srcPaths.css + 'main.scss')
       .pipe(plumber())
-      .pipe(sass.sync({
+      .pipe(gulpif(env === 'production', sass.sync({
+        outputStyle: 'compressed',
+        precision: 10,
+        includePaths: ['.'],
+      }), sass.sync({
         outputStyle: 'nested',
         precision: 10,
         includePaths: ['.'],
-      }))
-      .pipe(prefix({
+      })))
+      .pipe(gulpif(env === 'production', prefix({
+        browsers: [
+          'last 4 versions',
+          'android 4',
+          'opera 12',
+        ]
+      }), prefix({
         browsers: [
           'last 2 versions',
           'android 4',
           'opera 12',
         ]
-      }))
+      })))
       .pipe(dest(buildPaths.css));
-
-    // CSS Libs
-    // src(srcPaths.css + 'libs/*')
-    //   .pipe(plumber())
-    //   .pipe(cleanCSS())
-    //   .pipe(concatCSS('assets.css'))
-    //   .pipe(dest(buildPaths.css));
-
-    cb();
   }
 
 /*****************
@@ -129,6 +138,40 @@
     cb();
   }
 
+/*****************
+ * Images
+ *****************/
+  function images() {
+    return src(srcPaths.img + '**/**/*')
+      .pipe(gulpif(env === 'production', imagemin([
+        imagemin.gifsicle({
+          interlaced: true,
+          optimizationLevel: 3
+        }),
+        imagemin.jpegtran({
+          progressive: true
+        }),
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.svgo({
+          plugins: [{
+            removeViewBox: true,
+            removeComments: true,
+            removeMetadata: true,
+            removeTitle: true,
+            removeDesc: true,
+            cleanupAttrs: true
+          }]
+        })], {
+        verbose: true
+      }), imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 0 }),
+        imagemin.svgo({ plugins: [{ removeViewBox: true }] })
+      ])))
+      .pipe(dest(buildPaths.img))
+  }
+
  /*****************
  * Clean Build
  *****************/
@@ -140,8 +183,49 @@
 
 
 /*****************
+ * Watchers
+ *****************/
+  function watcher(cb) {
+    bs({
+      server: buildRoot,
+      files: [srcRoot + '**/**/**/**/*', buildRoot + '**/**/**/**/*'],
+    });
+
+    watch(srcRoot + '**/**/**/*.pug', views);
+    watch(srcPaths.css + '**/**/*.scss', styles);
+    watch(srcPaths.js + '**/**/*.js', scripts);
+    // watch(srcPaths.js + 'lib/**/*.js', ['jsLib']);
+    watch(srcPaths.assets + 'img/**/**', images);
+
+    cb();
+  }
+
+/*****************
+* Critic CSS
+*****************/
+  function critic() {
+    return src(buildRoot + '*.html')
+      .pipe(critical({
+        base: 'public/',
+        inline: true,
+        css: ['public/css/main.css']
+      }))
+      .pipe(dest('public'));
+  }
+
+/*****************
  * Gulp Exports
  *****************/
 exports.clean = cleanBuild;
-exports.dev = series(cleanBuild, views, styles, scripts, move);
-//exports.build = series();
+exports.views = views;
+exports.critic = critic;
+exports.dev = function(cb){
+  series(cleanBuild, parallel(views, styles, scripts, move, images), watcher)();
+  cb();
+}
+
+exports.build = function(cb){
+  series(cleanBuild, views, styles, scripts, move, critic, images)();
+  cb();
+}
+
